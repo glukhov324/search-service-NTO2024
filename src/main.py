5cdbc3ab0861e3eff48b8267e2c6cf9cf629f8ee
glukhov324 <glukhovv.nn@gmail.com>
2025-05-11 08:34:10 +0700

    add middlewares

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from src.api import router as main_router
 from src.settings import settings
+from src.middlewares import handle_exceptions_middleware
 
 app = FastAPI(title=settings.TITLE)
+app.middleware("http")(handle_exceptions_middleware)
 app.include_router(main_router)
---
+++ src/middlewares.py
@@ -0,0 +1,20 @@
+from fastapi import HTTPException, Request
+from fastapi.responses import JSONResponse
+
+from src.logger.main_logger import logger
+
+
+async def handle_exceptions_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception as error:
+        logger.warning(f"{error}")
+        undefined_error = HTTPException(500)
+
+        return JSONResponse(
+            content={
+                "code": 500,
+                "detail": str(error),
+            },
+            status_code=undefined_error.status_code,
+        )
